cc59db690352fbdf81a16a5c651d0ba1fbad89d9
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-20 16:24:37 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+class HelloWorld:
+    """
+    A simple class to print 'Hello World' to the console.
+    """
+
+    def print_hello_world(self):
+        """Prints 'Hello World' to the console."""
+        print("Hello World")
+
+def main():
+    """Main function to demonstrate HelloWorld usage."""
+    hello_world = HelloWorld()
+    hello_world.print_hello_world()
+
+if __name__ == "__main__":
+    main()
