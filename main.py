2ef09d2d916bfc5b2a1aeb03b4f4497292b8f138
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-21 16:34:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,5 @@ class HelloWorld:
         """Prints 'Hello World' to the console."""
         print("Hello World")
 
-def main():
-    """Main function to demonstrate HelloWorld usage."""
-    hello_world = HelloWorld()
-    hello_world.print_hello_world()
-
-if __name__ == "__main__":
+__main__":
     main()
