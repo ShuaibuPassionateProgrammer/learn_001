3ff421e3e12a5c51322dbfc75a0a216c06da3ffb
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-21 16:58:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 def is_even(num):
     return num % 2 == 0
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
